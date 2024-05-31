d288d7193afe238b1c411d8d4158e09a7e86661e
Ksenia <bogacevaksenia147@gmail.com>
2024-05-31 18:41:36 +1000

    исправлен фильтр

--- newspaper/news/templatetags/censor.py
+++ newspaper/news/templatetags/censor.py
@@ -5,6 +5,6 @@ register = template.Library()
 
 @register.filter()
 def censor(value):
-   censor_word = ["воображаемого", "способах"]
+   censor_word = ["воображаемого", "способах", "юните"]
 
    return ' '.join([i[0]+'*' * (len(i)-1) if i in censor_word else i for i in value.split()])
