4387e80f0ccf87a39aeac9f1e121d0a6156ce8e2
Ksenia <bogacevaksenia147@gmail.com>
2024-07-04 16:47:14 +1000

    Почта заработала

--- newspaper/newspaper/settings.py
+++ newspaper/newspaper/settings.py
@@ -149,8 +149,7 @@ EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_SSL = False
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 SITE_URL = 'http://127.0.0.1:8000'
@@ -173,3 +172,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+from django.core.mail import send_mail
+from django.conf import settings
+
+
